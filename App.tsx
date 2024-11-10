1e8444aaf77ac6cd3c3358d20e553ec4e0f27f25
yebun91 <yebun91@gmail.com>
2024-11-10 21:56:07 +0900

    27. FlatList를 통해 리스트 최적화하기

--- App.tsx
+++ App.tsx
@@ -1,14 +1,56 @@
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+type Goal = {
+  text: string;
+  id: string;
+};
 
 export default function App() {
+  const [value, setValue] = useState("");
+  const [goals, setGoals] = useState<Goal[]>([]);
+
+  const goalInputGandler = (text: string) => {
+    setValue(text);
+  };
+
+  const addGoalHandler = () => {
+    setGoals((prev) => [
+      ...prev,
+      { text: value, id: Math.random().toString() },
+    ]);
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="your course goal!" />
-        <Button title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="your course goal!"
+          onChangeText={goalInputGandler}
+          value={value}
+        />
+        <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
-        <Text>List of goals...</Text>
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={goals}
+          keyExtractor={(item) => item.id}
+          renderItem={(items) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalItemText}>{items.item.text}</Text>
+              </View>
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -16,17 +58,36 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 8,
+    borderRadius: 6,
+    backgroundColor: "#5e0acc",
+    padding: 8,
+  },
+  goalItemText: {
+    color: "white",
   },
 });
